9a59d6a5a3341e72bc14b2c9931e307db4f375f7
Stitch <mr0yun@qq.com>
2020-09-28 22:31:39 +0800

    add the file uname.c and relative files.

---
+++ week2/uname.c
@@ -0,0 +1,21 @@
+#include <sys/utsname.h>
+#include <unistd.h>
+#include <stdio.h>
+#include "err_exit.h"
+#include <sys/param.h>
+#include <limits.h>
+
+int main(){
+	struct utsname uts;
+	char hostname[MAXHOSTNAMELEN];
+	size_t size = MAXHOSTNAMELEN;
+	if(gethostname(hostname,size)!=0)
+		err_exit("Could not get hostname");
+	printf("hostname=%s\n", hostname);
+	if(uname(&uts) < 0)
+		err_exit("Could not get host information");
+	printf("System is %s on %s hardware\n", uts.sysname, uts.machine);
+	printf("Nodename is %s\n", uts.nodename);
+	printf("Release is %s, Version is %s\n", uts.release, uts.version);
+	exit(0);
+}
